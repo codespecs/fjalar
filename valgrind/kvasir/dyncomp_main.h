47e37cee0c95dcf3b89ac7b4635368bf303c32e0
pgbovine <pgbovine@csail.mit.edu>
2005-06-20 14:46:33 -0400

    Started implementing the newer, more elegant garbage collection algorithm (it now compiles, but I haven't run it)

--- valgrind/kvasir/dyncomp_main.h
+++ valgrind/kvasir/dyncomp_main.h
@@ -85,6 +85,9 @@ void val_uf_union_tags_in_range(Addr a, SizeT len);
 void val_uf_union_tags_at_addr(Addr a1, Addr a2);
 __inline__ UInt val_uf_find_leader(UInt tag);
 
+static void val_uf_make_set_for_tag(UInt tag, char saturate);
+
+
 extern VGA_REGPARM(1) UInt MC_(helperc_TAG_NOP) ( UInt );
 
 // Remember the special REGPARM(1) for the 64-bit case
