1e4bb2765816b3e25d2d3dc0cbd6ac8f32b78526
pgbovine <pgbovine@csail.mit.edu>
2005-06-22 16:03:35 -0400

    begin laying down the groundwork for reference counting implementation

--- valgrind/kvasir/union_find.c
+++ valgrind/kvasir/union_find.c
@@ -16,6 +16,8 @@ uf_name uf_find(uf_object *object) {
   // Path-compression:
   for(next=object->parent; next!=root; object=next, next=object->parent) {
     object->parent=root;
+    INC_REF_COUNT(root);
+    DEC_REF_COUNT(next);
   }
 
   return root;
@@ -25,6 +27,10 @@ void uf_make_set(uf_object *new_object, unsigned int t) {
   new_object->parent = new_object;
   new_object->rank = 0;
   new_object->tag = t;
+
+#ifdef USE_REF_COUNT
+  new_object->ref_count = 0;
+#endif
 }
 
 // Returns the new leader (uf_name)
@@ -44,11 +50,15 @@ uf_name uf_union(uf_object *obj1, uf_object *obj2) {
   }
 
   if(class1->rank < class2->rank) {
+    DEC_REF_COUNT(class1->parent);
     class1->parent = class2;
+    INC_REF_COUNT(class2);
     return class2;
   }
   else {
+    DEC_REF_COUNT(class2->parent);
     class2->parent = class1;
+    INC_REF_COUNT(class1);
     if(class1->rank == class2->rank) {
       (class1->rank)++;
     }
