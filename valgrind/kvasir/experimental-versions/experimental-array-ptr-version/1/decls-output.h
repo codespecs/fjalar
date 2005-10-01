0cd0cc3e46ec4a918f2039909328656dee76b4e0
pgbovine <pgbovine@csail.mit.edu>
2005-10-01 18:33:05 -0400

    Added an experimental version for a new technique to more precisely follow and print out pointer fields within array elements

---
+++ valgrind/kvasir/experimental-versions/experimental-array-ptr-version/1/decls-output.h
@@ -0,0 +1,131 @@
+/*
+   This file is part of Kvasir, a Valgrind skin that implements the
+   C language front-end for the Daikon Invariant Detection System
+
+   Copyright (C) 2004 Philip Guo, MIT CSAIL Program Analysis Group
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+*/
+
+/* decls-output.h:
+   Functions for creating .decls and .dtrace files and outputting
+   name and type information into a Daikon-compatible .decls file
+*/
+
+#ifndef DECLS_OUTPUT_H
+#define DECLS_OUTPUT_H
+
+#include "generate_daikon_data.h"
+#include "disambig.h"
+#include <stdio.h>
+
+#define MAXIMUM_ARRAY_SIZE_TO_EXPAND 10
+
+// The stack which represents the full name of the variable
+// that we currently want to print out
+#define MAX_STRING_STACK_SIZE 100
+
+int g_daikonVarIndex;
+
+char* fullNameStack[MAX_STRING_STACK_SIZE];
+int fullNameStackSize;
+
+struct genhashtable* g_compNumberMap;
+int g_curCompNumber;
+
+// For use by printOneDaikonVariableAndDerivatives
+typedef enum VariableOrigin {
+  DERIVED_VAR, // Always switches to this after one recursive call
+  DERIVED_FLATTENED_ARRAY_VAR, // A derived variable as a result of flattening an array
+  GLOBAL_VAR,
+  FUNCTION_ENTER_FORMAL_PARAM,
+  FUNCTION_EXIT_FORMAL_PARAM,
+  FUNCTION_RETURN_VAR // Assume for function exits only
+} VariableOrigin;
+
+// For use by printVariablesInVarList() and other functions
+typedef enum OutputFileType {
+  DECLS_FILE,
+  DTRACE_FILE,
+  DISAMBIG_FILE,
+  DYNCOMP_EXTRA_PROP, // only for DynComp
+  FAUX_DECLS_FILE     // only for DynComp - temporarily redirect decls_fp to '/dev/null'
+} OutputFileType;
+
+// For use by vars_tree:
+typedef struct {
+  char* function_daikon_name;
+  char* function_variables_tree; // A GNU binary tree of variable names (strings)
+} FunctionTree;
+
+const char* ENTRY_DELIMETER;
+const char* GLOBAL_STRING;
+const char* ENTER_PPT;
+const char* EXIT_PPT;
+
+extern int MAX_NUM_STRUCTS_TO_DEREFERENCE;
+
+void stringStackPush(char** stringStack, int* stringStackSizePtr, char* str);
+char* stringStackPop(char** stringStack, int* stringStackSizePtr);
+char* stringStackTop(char** stringStack, int stringStackSize);
+void stringStackClear(int* stringStackSizePtr);
+int stringStackStrLen(char** stringStack, int stringStackSize);
+char* strdupFullNameString(char** stringStack, int stringStackSize);
+char* strdupFullNameStringReverse(char** stringStack, int stringStackSize);
+
+char createDeclsAndDtraceFiles(char* appname);
+char splitDirectoryAndFilename(const char* input, char** dirnamePtr, char** filenamePtr);
+
+void printDeclsHeader();
+void printOneFunctionDecl(DaikonFunctionInfo* funcPtr, char isEnter, char faux_decls);
+
+void printAllFunctionDecls(char faux_decls);
+void printAllObjectAndClassDecls();
+
+int compareStrings(const void *a, const void *b);
+//int compareUInts(const void *a, const void *b);
+void initializeProgramPointsTree();
+
+char prog_pts_tree_entry_found(DaikonFunctionInfo* cur_entry);
+
+int compareFunctionTrees(const void *a, const void *b);
+void initializeVarsTree();
+
+void outputDeclsFile(char faux_decls);
+void DC_outputDeclsAtEnd();
+void openTheDtraceFile(void);
+
+void printVariablesInVarList(DaikonFunctionInfo* funcPtr,
+                             char isEnter,
+			     VariableOrigin varOrigin,
+			     char* stackBaseAddr,
+			     OutputFileType outputType,
+			     char allowVarDumpToFile,
+			     char* trace_vars_tree,
+                             char printClassProgramPoint,
+                             char stopAfterFirstVar);
+
+void outputDaikonVar(DaikonVariable* var,
+		     VariableOrigin varOrigin,
+		     int numDereferences,
+		     char isAlreadyDaikonSequence,
+		     char stopExpandingArrays,
+		     char stopDerivingMemberVars,
+		     char allowVarDumpToFile,
+		     char* trace_vars_tree, // Binary tree within FunctionTree struct
+		     OutputFileType outputType,
+		     DisambigOverride disambigOverride, // Only relevant for .disambig
+		     // only valid if isDtraceFilePrint:
+		     void* basePtrValue,
+		     char overrideIsInitialized,
+		     char isDummy,
+		     unsigned long upperBound,
+		     unsigned long bytesBetweenElts,
+		     char structParentAlreadySetArrayInfo,
+                     int numStructsDereferenced,
+                     DaikonFunctionInfo* varFuncInfo, char isEnter);
+
+#endif
---
+++ valgrind/kvasir/experimental-versions/experimental-array-ptr-version/5/kvasir_main.h
@@ -0,0 +1,161 @@
+/*
+   This file is part of Kvasir, a Valgrind skin that implements the
+   C language front-end for the Daikon Invariant Detection System
+
+   Copyright (C) 2004 Philip Guo, MIT CSAIL Program Analysis Group
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+*/
+
+/* kvasir_main.h:
+   This file contains mostly prototypes that interface with
+   the Memcheck skin code (most notably, mc_main.c).
+*/
+
+#ifndef KVASIR_MAIN_H
+#define KVASIR_MAIN_H
+
+// Comment this out when we want to release Kvasir to end-users:
+// #define KVASIR_DEVEL_BUILD
+
+#include <assert.h>
+#include "tool.h"
+#include "kvasir_runtime.h"
+#include "mc_translate.h"
+
+// Global variables that are set by command-line options
+char* kvasir_decls_filename;
+char* kvasir_dtrace_filename;
+Bool kvasir_with_dyncomp;
+Bool kvasir_dyncomp_no_gc;
+Bool kvasir_dyncomp_fast_mode;
+Bool kvasir_print_debug_info;
+Bool kvasir_ignore_globals;
+Bool kvasir_ignore_static_vars;
+Bool kvasir_dtrace_append;
+Bool kvasir_dtrace_gzip;
+Bool kvasir_output_fifo;
+Bool kvasir_asserts_aborts_on;
+Bool kvasir_decls_only;
+Bool kvasir_limit_static_vars;
+Bool kvasir_default_disambig;
+Bool kvasir_smart_disambig;
+Bool kvasir_use_bit_level_precision;
+Bool kvasir_output_struct_vars;
+Bool kvasir_repair_format;
+Bool kvasir_flatten_arrays;
+Bool kvasir_disambig_ptrs;
+Bool dyncomp_print_debug_info;
+Bool dyncomp_separate_entry_exit_comp;
+int kvasir_array_length_limit;
+// These are used as both strings and boolean flags -
+// They are initialized to 0 upon initiation so if they are
+// never filled with values by the respective command-line
+// options, then they can be treated as False
+char* kvasir_dump_prog_pt_names_filename;
+char* kvasir_dump_var_names_filename;
+char* kvasir_trace_prog_pts_filename;
+char* kvasir_trace_vars_filename;
+char* kvasir_disambig_filename;
+char *kvasir_program_stdout_filename;
+char *kvasir_program_stderr_filename;
+int dyncomp_gc_after_n_tags;
+
+Bool actually_output_separate_decls_dtrace;
+
+Bool dyncomp_without_dtrace;
+
+// Turn this off to not have DPRINTFs expand to anything, just in
+// case you want to optimize for performance,
+// but leave it on if you want the --debug command-line option to work
+#define USE_DPRINTFS
+
+#ifdef USE_DPRINTFS
+#define DPRINTF(...) do { if (kvasir_print_debug_info) \
+      VG_(printf)(__VA_ARGS__); } while (0)
+
+#define DYNCOMP_DPRINTF(...) do { if (kvasir_with_dyncomp && dyncomp_print_debug_info) \
+      VG_(printf)(__VA_ARGS__); } while (0)
+#endif
+
+// Define these to expand to nothing if USE_DPRINTFS is off
+#ifndef USE_DPRINTFS
+#define DPRINTF(...)
+#define DYNCOMP_DPRINTF(...)
+#endif
+
+#define DABORT(...) do { if (kvasir_asserts_aborts_on) { \
+      VG_(printf)(__VA_ARGS__); abort();} } while (0)
+
+#define DASSERT(target) do { if (kvasir_asserts_aborts_on) \
+      tl_assert(target); } while (0)
+
+void handle_possible_entry(MCEnv* mce, Addr64 addr);
+void handle_possible_exit(MCEnv* mce, IRJumpKind jk);
+
+extern VGA_REGPARM(2) void enter_function(Char* daikon_name, Addr StartPC);
+extern VGA_REGPARM(1) void exit_function(Char* daikon_name);
+
+void check_ESP();
+
+void kvasir_pre_clo_init();
+void kvasir_post_clo_init();
+void kvasir_finish();
+
+void printFunctionEntryStack();
+void kvasir_print_usage();
+Bool kvasir_process_cmd_line_option(Char* arg);
+
+
+/*
+Requires:
+Modifies: fn_stack[ fn_stack_top - 1 ].lowestESP
+Returns:
+Effects: Compares the current ESP with the lowestESP from the current
+         function and sets lowestESP to current ESP if current ESP
+         is lower.  This will provide an indicator of how far down
+         the function has ever reached on the stack.
+
+This is called from hooks within mac_shared.h
+
+  //Okay, this is called everytime somebody tries to allocate space
+  //on the stack, but the function on the top of the stack is the
+  //function that we are recording.  The problem is that if the
+  //function that we are recording calls another function that
+  //we do not record, that new function will mess around with the
+  //stack all it wants, but those values aren't accurately reflected
+  //by the lowestESP of the function we are currently recording.
+  //For example, if my function "foo" drives the stack down to 100,000
+  //and then it calls printf which calls other crap, driving the stack
+  //down to 50,000, as far as foo is concerned, it only drove
+  //the stack down to 100,000 but check_ESP() thinks it drove the
+  //stack down to 50,000.
+
+  //AD HOC solution: DON'T DEAL WITH THIS AT ALL!!!
+*/
+// Macro version to improve speed:
+// (Remember that this code will be inserted in mc_main.c so it needs to have
+//  the proper extern variables declared.)
+#define CHECK_ESP(currentESP)                                           \
+   if (fn_stack_top > 0) {                                              \
+      FunctionEntry* curFunc = &(fn_stack[ fn_stack_top - 1]);          \
+      if (currentESP < curFunc->lowestESP) {                            \
+         fn_stack[ fn_stack_top - 1].lowestESP = currentESP;            \
+      }                                                                 \
+   }
+
+// Slower because we need to explicitly get the ESP
+#define CHECK_ESP_SLOW()                                                \
+   if (fn_stack_top > 0) {                                              \
+      FunctionEntry* curFunc = &(fn_stack[ fn_stack_top - 1]);          \
+      Addr  currentESP = VG_(get_SP)(VG_(get_running_tid)());           \
+      if (currentESP < curFunc->lowestESP) {                            \
+         fn_stack[ fn_stack_top - 1].lowestESP = currentESP;            \
+      }                                                                 \
+   }
+
+
+#endif
