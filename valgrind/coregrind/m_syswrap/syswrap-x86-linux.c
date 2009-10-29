0ff26dac899e67a4bd5289b91d4eb6b8da568765
falaina <falaina@falaina.net>
2009-10-29 15:10:30 -0400

    Valgrind merge from revision 10896 to 10921.  VEX IR merge from 1920 to 1920.

--- valgrind/coregrind/m_syswrap/syswrap-x86-linux.c
+++ valgrind/coregrind/m_syswrap/syswrap-x86-linux.c
@@ -2253,8 +2253,13 @@ const SyscallTableEntry ML_(syscall_table)[] = {
    LINXY(__NR_epoll_create1,     sys_epoll_create1),     // 329
 
    //   (__NR_dup3,              sys_ni_syscall)        // 330
-   LINXY(__NR_pipe2,             sys_pipe2)             // 331
-   //   (__NR_inotify_init1,     sys_ni_syscall)        // 332
+   LINXY(__NR_pipe2,             sys_pipe2),            // 331
+   LINXY(__NR_inotify_init1,     sys_inotify_init1),    // 332
+   LINXY(__NR_preadv,            sys_preadv),           // 333
+   LINX_(__NR_pwritev,           sys_pwritev),          // 334
+
+   //   (__NR_rt_tgsigqueueinfo, sys_ni_syscall)        // 335
+   LINXY(__NR_perf_counter_open, sys_perf_counter_open) // 336
 };
 
 const UInt ML_(syscall_table_size) = 
