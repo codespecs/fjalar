991cee95e1868382eeea56ddf7dcd48c6703bbc2
Michael Ernst <mernst@cs.washington.edu>
2015-12-09 20:04:41 -0800

    Set DAIKONDIR environment variable

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -v
 
-# Temporarily remove "test", until Valgrind changes from upstream are integrated
+export DAIKONDIR=`pwd`/../daikon
+
+# Temporarily remove "test", until Valgrind changes from upstream are integrated.
 # make build test daikon-test
 make build daikon-test
