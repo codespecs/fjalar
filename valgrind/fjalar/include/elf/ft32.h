4bfb24bf8250663768e89ae0b8f5d3e422c8560f
Michael Ernst <mernst@cs.washington.edu>
2016-08-05 15:46:15 -0700

    Upgrade to binutils version 2.26.1

--- valgrind/fjalar/include/elf/ft32.h
+++ valgrind/fjalar/include/elf/ft32.h
@@ -1,5 +1,5 @@
-/* d30v ELF support for BFD.
-   Copyright (C) 1998-2015 Free Software Foundation, Inc.
+/* ft32 ELF support for BFD.
+   Copyright (C) 2009-2015 Free Software Foundation, Inc.
 
    This file is part of BFD, the Binary File Descriptor library.
 
@@ -17,26 +17,21 @@
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street - Fifth Floor, Boston, MA 02110-1301, USA.  */
 
-#ifndef _ELF_D30V_H
-#define _ELF_D30V_H
+#ifndef _ELF_FT32_H
+#define _ELF_FT32_H
 
 #include "elf/reloc-macros.h"
 
-/* Relocations.  */
-START_RELOC_NUMBERS (elf_d30v_reloc_type)
-  RELOC_NUMBER (R_D30V_NONE, 0)
-  RELOC_NUMBER (R_D30V_6, 1)
-  RELOC_NUMBER (R_D30V_9_PCREL, 2)
-  RELOC_NUMBER (R_D30V_9_PCREL_R, 3)
-  RELOC_NUMBER (R_D30V_15, 4)
-  RELOC_NUMBER (R_D30V_15_PCREL, 5)
-  RELOC_NUMBER (R_D30V_15_PCREL_R, 6)
-  RELOC_NUMBER (R_D30V_21, 7)
-  RELOC_NUMBER (R_D30V_21_PCREL, 8)
-  RELOC_NUMBER (R_D30V_21_PCREL_R, 9)
-  RELOC_NUMBER (R_D30V_32, 10)
-  RELOC_NUMBER (R_D30V_32_PCREL, 11)
-  RELOC_NUMBER (R_D30V_32_NORMAL, 12)
-END_RELOC_NUMBERS (R_D30V_max)
+/* Relocation types.  */
+START_RELOC_NUMBERS (elf_ft32_reloc_type)
+  RELOC_NUMBER (R_FT32_NONE, 0)
+  RELOC_NUMBER (R_FT32_32, 1)
+  RELOC_NUMBER (R_FT32_16, 2)
+  RELOC_NUMBER (R_FT32_8,  3)
+  RELOC_NUMBER (R_FT32_10, 4)
+  RELOC_NUMBER (R_FT32_20, 5)
+  RELOC_NUMBER (R_FT32_17, 6)
+  RELOC_NUMBER (R_FT32_18, 7)
+END_RELOC_NUMBERS (R_FT32_max)
 
-#endif
+#endif /* _ELF_FT32_H */
