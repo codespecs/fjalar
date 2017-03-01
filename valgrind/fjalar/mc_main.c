5028cb1e42af1d9cafe1e55621f1e9c92d1f3f13
Mark Roberts <markro@cs.washington.edu>
2017-03-01 14:01:16 -0800

    Change version to 5.5.2

--- valgrind/fjalar/mc_main.c
+++ valgrind/fjalar/mc_main.c
@@ -8017,7 +8017,7 @@ static void mc_pre_clo_init(void)
    VG_(details_name)            ("kvasir");
    /* This next line is automatically updated by the toplevel Daikon
       distribution Makefile; be careful with its formatting -SMcC */
-   VG_(details_version)         ("5.5.1");
+   VG_(details_version)         ("5.5.2");
    VG_(details_description)     ("C/C++ Language Front-End for Daikon with DynComp comparability analysis tool.");
    VG_(details_copyright_author)(
    "Copyright (C) 2007-2016, University of Washington CSE PLSE Group");
