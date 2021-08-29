45e84fdfaa1b6fc0b59717ce2ad390243d034268
Mark Roberts <markro49@users.noreply.github.com>
2021-08-29 13:51:37 -0700

    upgrade binutils to 2.36.1 (#53)

--- valgrind/fjalar/bucomm.h
+++ valgrind/fjalar/bucomm.h
@@ -1,5 +1,5 @@
 /* bucomm.h -- binutils common include file.
-   Copyright (C) 1991-2020 Free Software Foundation, Inc.
+   Copyright (C) 1991-2021 Free Software Foundation, Inc.
 
    This file is part of GNU Binutils.
 
@@ -51,7 +51,7 @@ int display_info (void);
 
 void print_arelt_descr (FILE *, bfd *, bfd_boolean, bfd_boolean);
 
-char *make_tempname (const char *);
+char *make_tempname (const char *, int *);
 char *make_tempdir (const char *);
 
 bfd_vma parse_vma (const char *, const char *);
--- valgrind/fjalar/mc_translate.c
+++ valgrind/fjalar/mc_translate.c
@@ -32,7 +32,7 @@
    This file is part of Fjalar, a dynamic analysis framework for C/C++
    programs.
 
-   Copyright (C) 2007-2020 University of Washington Computer Science & Engineering Department,
+   Copyright (C) 2007-2021 University of Washington Computer Science & Engineering Department,
    Programming Languages and Software Engineering Group
 
    Copyright (C) 2004-2006 Philip Guo (pgbovine@alum.mit.edu),
