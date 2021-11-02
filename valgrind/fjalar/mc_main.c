5713b223c3a59364f35dab134ce7dbcca7eec607
Mark Roberts <markro@cs.washington.edu>
2021-11-02 06:55:56 -0700

    Bump version number for ongoing development.

--- valgrind/fjalar/mc_main.c
+++ valgrind/fjalar/mc_main.c
@@ -8344,7 +8344,7 @@ static void mc_pre_clo_init(void)
    VG_(details_name)            ("kvasir");
    /* This next line is automatically updated by the toplevel Daikon
       distribution Makefile; be careful with its formatting -SMcC */
-   VG_(details_version)         ("5.8.10");
+   VG_(details_version)         ("5.8.11");
    VG_(details_description)     ("C/C++ Language Front-End for Daikon with DynComp comparability analysis tool.");
    VG_(details_copyright_author)(
       "Copyright (C) 2007-2021, University of Washington CSE PLSE Group");
